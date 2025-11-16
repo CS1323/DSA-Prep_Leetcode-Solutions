32ed3641e30ab972853c9e0c6e2f96553ebc6d09
Cheyenne Sterbick <sterbickc@gmail.com>
2025-11-15 19:37:07 -0800

    perf: optimize Leetcode 1186 solution
    Maximum Subarray Sum with One Deletion
    Practice for DP. Reduced space complexity to constant from linear.

--- 1186_MaximumSubarraySumWithOneDeletion.py
+++ 1186_MaximumSubarraySumWithOneDeletion.py
@@ -2,28 +2,31 @@ def maximumSum(arr) -> int:
 
     n = len(arr)
 
-    # dp_no_del[i]  = max subarray sum ending at i with NO deletion used
-    # dp_with_del[i]= max subarray sum ending at i WITH one deletion used
-    dp_no_del = [0] * n
-    dp_with_del = [0] * n
+    # dp_no_del  = max subarray sum with NO deletion used
+    # dp_with_del= max subarray sum WITH one deletion used
+    # base cases:
+    dp_no_del = arr[0]
+    dp_with_del = float("-inf")  # cannot delete the only element and still end at index 0
 
-    # base cases
-    dp_no_del[0] = arr[0]
-    dp_with_del[0] = float("-inf")  # cannot delete the only element and still end at index 0
+    max_sum = arr[0]
 
     for i in range(1, n):
 
+        prev_no_del = dp_no_del
+
         # start new subarray, or extend previous subarray (Kadane's)
-        dp_no_del[i] = max(arr[i], dp_no_del[i-1] + arr[i])         
+        dp_no_del = max(arr[i], prev_no_del + arr[i])         
 
         # delete curr element, or extend subarray w/earlier deletion
-        dp_with_del[i] = max(dp_no_del[i-1], dp_with_del[i-1] + arr[i]) 
+        dp_with_del = max(prev_no_del, dp_with_del + arr[i]) 
+
+        max_sum = max(max_sum, dp_no_del, dp_with_del)
 
     # max subarray sum with or without deletion
-    return max(max(dp_no_del), max(dp_with_del))
+    return max_sum
 
     # Time:  O(n)
-    # Space: O(n)
+    # Space: O(1)
 
 arr = [2,-1,-1,10]
 print(maximumSum(arr))
